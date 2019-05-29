d9230cd54f01b0e114e088bbf5fb38477003e2f5
H294067 <vinay.kr@honeywell.com>
2019-05-29 15:45:31 +0530

    added nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'home',
+  component: HomeComponent
+},
+{
+  path:'portfolio',
+  component: PortfolioComponent
+},
+{
+  path:'contact',
+  component: ContactComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
